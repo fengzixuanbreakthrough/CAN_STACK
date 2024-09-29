48817b78d7ef06edac1588f606c6ceb6e80f9477
冯梓轩 <10498276+feng-zixuanren@user.noreply.gitee.com>
2024-09-29 23:13:26 +0800

    First Commit

---
+++ Appl/GenData/ContractPhase/Mcu/SchM_Mcu_Type.h
@@ -0,0 +1,35 @@
+/**********************************************************************************************************************
+ *  COPYRIGHT
+ *  -------------------------------------------------------------------------------------------------------------------
+ *
+ *                This software is copyright protected and proprietary to Vector Informatik GmbH.
+ *                Vector Informatik GmbH grants to you only those rights as set out in the license conditions.
+ *                All other rights remain with Vector Informatik GmbH.
+ *  -------------------------------------------------------------------------------------------------------------------
+ *  FILE DESCRIPTION
+ *  -------------------------------------------------------------------------------------------------------------------
+ *          File:  SchM_Mcu_Type.h
+ *        Config:  E:\\x{8D44}\x{6599}\\x{6C7D}\x{8F66}\AutoSAR\01.AUTOSAR\x{5B66}\x{4E60}\x{8D44}\x{6599}\S32K144 AUTOSAR\x{5DE5}\x{5177}\x{5305}\share_20230823\AUTOSAR_TOOLs\S32K144_SIP\MICROSAR\CBD1800257_D01_S32K1xx\Applications\S32K144_Start_new_IAR/"E:/\x{8D44}\x{6599}/\x{6C7D}\x{8F66}/AutoSAR/01.AUTOSAR\x{5B66}\x{4E60}\x{8D44}\x{6599}/S32K144 AUTOSAR\x{5DE5}\x{5177}\x{5305}/share_20230823/AUTOSAR_TOOLs/S32K144_SIP/MICROSAR/CBD1800257_D01_S32K1xx/Applications/S32K144_Start_new_IAR/S32K144_Start.dpa"
+ *    BSW Module:  Mcu
+ *  Generated at:  Thu Apr 18 23:12:17 2024
+ *
+ *     Generator:  MICROSAR RTE Generator Version 4.19.0
+ *                 RTE Core Version 1.19.0
+ *       License:  CBD1800257
+ *
+ *   Description:  Module Interlink Types Header for BSW Module <Mcu> (Contract Phase)
+ *********************************************************************************************************************/
+#ifndef SCHM_MCU_TYPE_H
+# define SCHM_MCU_TYPE_H
+
+# ifdef __cplusplus
+extern "C" {
+# endif  /* __cplusplus */
+
+# include "Rte_Type.h"
+
+# ifdef __cplusplus
+} /* extern "C" */
+# endif  /* __cplusplus */
+
+#endif /* SCHM_MCU_TYPE_H */
---
+++ Appl/Source/CtLedTask/Rte_CtLedTask_Type.h
@@ -0,0 +1,55 @@
+/**********************************************************************************************************************
+ *  COPYRIGHT
+ *  -------------------------------------------------------------------------------------------------------------------
+ *
+ *                This software is copyright protected and proprietary to Vector Informatik GmbH.
+ *                Vector Informatik GmbH grants to you only those rights as set out in the license conditions.
+ *                All other rights remain with Vector Informatik GmbH.
+ *  -------------------------------------------------------------------------------------------------------------------
+ *  FILE DESCRIPTION
+ *  -------------------------------------------------------------------------------------------------------------------
+ *          File:  Rte_CtLedTask_Type.h
+ *        Config:  E:\\x{8D44}\x{6599}\\x{6C7D}\x{8F66}\AutoSAR\01.AUTOSAR\x{5B66}\x{4E60}\x{8D44}\x{6599}\S32K144 AUTOSAR\x{5DE5}\x{5177}\x{5305}\share_20230823\AUTOSAR_TOOLs\S32K144_SIP\MICROSAR\CBD1800257_D01_S32K1xx\Applications\S32K144_Start_new_IAR/"E:/\x{8D44}\x{6599}/\x{6C7D}\x{8F66}/AutoSAR/01.AUTOSAR\x{5B66}\x{4E60}\x{8D44}\x{6599}/S32K144 AUTOSAR\x{5DE5}\x{5177}\x{5305}/share_20230823/AUTOSAR_TOOLs/S32K144_SIP/MICROSAR/CBD1800257_D01_S32K1xx/Applications/S32K144_Start_new_IAR/S32K144_Start.dpa"
+ *     SW-C Type:  CtLedTask
+ *  Generated at:  Thu Apr 18 23:12:16 2024
+ *
+ *     Generator:  MICROSAR RTE Generator Version 4.19.0
+ *                 RTE Core Version 1.19.0
+ *       License:  CBD1800257
+ *
+ *   Description:  Application types header file for SW-C <CtLedTask> (Contract Phase)
+ *********************************************************************************************************************/
+
+/* double include prevention */
+#ifndef _RTE_CTLEDTASK_TYPE_H
+# define _RTE_CTLEDTASK_TYPE_H
+
+# ifdef __cplusplus
+extern "C"
+{
+# endif /* __cplusplus */
+
+# include "Rte_Type.h"
+
+/**********************************************************************************************************************
+ * Range, Invalidation, Enumeration and Bit Field Definitions
+ *********************************************************************************************************************/
+
+# ifndef COMM_NO_COMMUNICATION
+#  define COMM_NO_COMMUNICATION (0U)
+# endif
+
+# ifndef COMM_SILENT_COMMUNICATION
+#  define COMM_SILENT_COMMUNICATION (1U)
+# endif
+
+# ifndef COMM_FULL_COMMUNICATION
+#  define COMM_FULL_COMMUNICATION (2U)
+# endif
+
+
+# ifdef __cplusplus
+} /* extern "C" */
+# endif /* __cplusplus */
+
+#endif /* _RTE_CTLEDTASK_TYPE_H */
